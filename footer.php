873ddf99f3163554921d8346987a1f34397f8d4a
Lee Jordan <ldjordan@gmail.com>
2014-12-19 15:15:44 +0000

    footer tweaks

--- footer.php
+++ footer.php
@@ -11,25 +11,29 @@
     <?php $currentTitle = get_the_title(); ?>
     <?php $args = array( 'numberposts' => '100' ); ?>
     <?php $recent_posts = get_posts( $args ); ?>
-    <form id="further-reading" action="" method="post">
-        <label for="browse">
-            Browse posts: 
-        </label>
-        <select name="browse" id="browse">
-            <?php foreach ($recent_posts as $post) : ?>
-                <?php if (is_single()) : ?>
-                    <option value="<?php the_permalink(); ?>"
-                        <?php if ($currentTitle === get_the_title()) : ?>
-                            selected="selected"
-                        <?php endif ?>
-                        >
-                        <?php the_title(); ?>
-                    </option>
-                <? endif ?>
-            <? endforeach ?>
-        </select>
-        <input type="submit" value="Go" id="browse-submit" />
-    </form>
+    <div class="footer-container">
+        <form id="further-reading" action="" method="post">
+            <div class="form-group">
+                <label for="browse">
+                    Browse posts:
+                </label>
+                <select name="browse" id="browse">
+                    <?php foreach ($recent_posts as $post) : ?>
+                        <?php if (is_single()) : ?>
+                            <option value="<?php the_permalink(); ?>"
+                                <?php if ($currentTitle === get_the_title()) : ?>
+                                    selected="selected"
+                                <?php endif ?>
+                                >
+                                <?php the_title(); ?>
+                            </option>
+                        <? endif ?>
+                    <? endforeach ?>
+                </select>
+                <input type="submit" value="Go" id="browse-submit" />
+            </div>
+        </form>
+    </div>
 
 <?php endif ?>
 
